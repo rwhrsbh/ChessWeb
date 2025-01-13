04d801e6c43b62c72b42b94bbfab5e1fdcd2e3e2
Maxym Zhyltsov <140576663+rwhrsbh@users.noreply.github.com>
2025-01-13 23:55:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Раздача статических файлов
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Маршрут для главной страницы
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Создаем HTTP сервер
