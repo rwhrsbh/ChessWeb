314109af87c9eb6e87a1c683df6bd4b02a0fa4f1
Maxym Zhyltsov <140576663+rwhrsbh@users.noreply.github.com>
2025-01-13 23:58:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ChessEngine = require('./chess-engine');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Раздача статических файлов
 app.use(express.static(path.join(__dirname)));
